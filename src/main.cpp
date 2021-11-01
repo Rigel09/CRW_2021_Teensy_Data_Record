5bd5136b1dd89e6673e036be2dfaf7af8bea4d71
JNewton <jnnewton@hiwaay.net>
2021-10-31 23:15:38 -0500

    Added SD Card Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
+/*
+Retrieves sensor data and writes to a csv file on the sd card
+Sensors:
+  Magnetometer x1
+  Accelerometer x3
+  Gyroscope x3
+*/
+
 #include "Arduino.h"
 
 void setup()
 {
-  pinMode(LED_BUILTIN,OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  digitalWriteFast(LED_BUILTIN,!digitalReadFast(LED_BUILTIN));
-  delay(500);
+    digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
+    delay(500);
 }
