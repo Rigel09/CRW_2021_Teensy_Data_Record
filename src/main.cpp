9c0059f006513b93ac23104c713bd2cb331b1d51
JNewton <jnnewton@hiwaay.net>
2021-11-01 22:00:14 -0500

    Added documentation for SDFat Lib

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,52 @@ Sensors:
   Magnetometer x1
   Accelerometer x3
   Gyroscope x3
+
+Author: Charger Rocket Works 2021/2022 Team
+Date: Fall 2021
 */
 
 #include "Arduino.h"
+#include "SPI.h"
+#include "SdFat.h"
+
+// Use built-in SD for SPI modes on Teensy 3.5/3.6.
+// Teensy 4.0 use first SPI port.
+// SDCARD_SS_PIN is defined for the built-in SD on some boards.
+#ifndef SDCARD_SS_PIN
+const uint8_t SD_CS_PIN = SS;
+#else  // SDCARD_SS_PIN
+// Assume built-in SD is used.
+const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
+#endif // SDCARD_SS_PIN
+
+// SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
+// 1 for FAT16/FAT32, 2 for exFAT, 3 for FAT16/FAT32 and exFAT.
+#define SD_FAT_TYPE 3
+
+// Try to select the best SD card configuration.
+#if HAS_SDIO_CLASS
+#  define SD_CONFIG SdioConfig(FIFO_SDIO)
+#elif ENABLE_DEDICATED_SPI
+#  define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_CLOCK)
+#else // HAS_SDIO_CLASS
+#  define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
+#endif // HAS_SDIO_CLASS
+
+#define testFileName "testFile.csv"
+
+// Sd card class
+SdFs sd;
+FsFile testFile;
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-    digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
-    delay(500);
+  digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
+  delay(500);
 }
